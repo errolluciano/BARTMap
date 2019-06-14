e7f3190b23aa293c3863a8698c9077d611a6b451
erroll <erroll.luciano@gmail.com>
2019-06-13 22:24:37 -0700

    Minor changes

--- BARTMap/BartAPI.swift
+++ BARTMap/BartAPI.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-enum Method: String{
+enum Command: String {
     case depart = "depart"
     case arrive = "arrive"
     case fare = "fare"
@@ -20,14 +20,14 @@ struct BartAPI {
     private static let baseURLString = "http://api.bart.gov/api/sched.aspx"
     private static let apiKey = "MW9S-E7SL-26DU-VV8V"
     
-    private static func flickrURL(method: Method, parameters: [String: String]?) -> URL {
+    private static func flickrURL(command: Command, parameters: [String: String]?) -> URL {
         
         var components = URLComponents(string: baseURLString)!
         
         var queryItems = [URLQueryItem]()
         
         let baseParams = [
-            "cmd": method.rawValue,
+            "cmd": command.rawValue,
             "key": apiKey,
             "json": "y"
         ]
@@ -43,6 +43,7 @@ struct BartAPI {
                 queryItems.append(item)
             }
         }
+        
         components.queryItems = queryItems
         
         return components.url!
